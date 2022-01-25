4d4d823445a1811c50567c4816a1603711812973
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-01-25 13:28:07 +0300

    update telescope themes

--- dot_config/nvim/lua/settings/telescope.lua
+++ dot_config/nvim/lua/settings/telescope.lua
@@ -4,14 +4,17 @@ local telescope = setmetatable({}, {
     __index = function(_, k) return require('telescope.builtin')[k] end,
 })
 
+local themes =
+    setmetatable({}, {__index = function(_, k) return require('telescope.themes')[k] end})
+
 function M.mappings()
-    MAP.nnoremap('<C-p>', function() telescope.find_files {} end)
-    MAP.nnoremap('<C-b>', function() telescope.buffers {shorten_path = false} end)
-    MAP.nnoremap('<leader>ff', function() telescope.find_files {} end)
-    MAP.nnoremap('<leader>fg',
-                 function() telescope.live_grep(require('telescope.themes').get_ivy({})) end)
-    MAP.nnoremap('<leader>fb', function() telescope.buffers {shorten_path = false} end)
-    MAP.nnoremap('<leader>fw', function() telescope.grep_string {} end)
+    MAP.nnoremap('<C-p>', function() telescope.find_files(themes.get_ivy({})) end)
+    MAP.nnoremap('<C-b>', function() telescope.buffers(themes.get_ivy({shorten_path = false})) end)
+    MAP.nnoremap('<leader>ff', function() telescope.find_files(themes.get_ivy({})) end)
+    MAP.nnoremap('<leader>fb',
+                 function() telescope.buffers(themes.get_ivy({shorten_path = false})) end)
+    MAP.nnoremap('<leader>fg', function() telescope.live_grep(themes.get_ivy({})) end)
+    MAP.nnoremap('<leader>fw', function() telescope.grep_string(themes.get_ivy({})) end)
     MAP.nnoremap('<leader>gs', function()
         telescope.git_status {winblend = 10, border = true, previewer = false, shorten_path = false}
     end)
