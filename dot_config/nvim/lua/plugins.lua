af203dbc417805db38f10c0b9cbfead709dfe267
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-01-30 19:01:20 +0300

    remove FixCursorHold (breaks lsp codelens)

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -20,10 +20,6 @@ return packer.startup({
         -- extended repeat ('.') for another plugins
         use 'tpope/vim-repeat'
 
-        use {'antoinemadec/FixCursorHold.nvim', run = function()
-            vim.g.curshold_updatime = 1000
-        end}
-
         use {
             'neovim/nvim-lspconfig',
             requires = {
