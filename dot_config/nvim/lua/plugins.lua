8010219af630b5ab296fa9d892959fd9b35f58c5
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-02-16 20:44:00 +0300

    neovim: telescope: add Ctrl+<Up>/Ctrl+<Down> mappings for history movements

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -149,6 +149,17 @@ return packer.startup({
             module = 'telescope',
             setup = function() require('settings.telescope').setup() end,
             config = function()
+                require('telescope').setup({
+                    defaults = {
+                        mappings = {
+                            i = {
+                                ['<C-Down>'] = require('telescope.actions').cycle_history_next,
+                                ['<C-Up>'] = require('telescope.actions').cycle_history_prev,
+                            },
+                        },
+                    },
+                })
+
                 require('telescope').load_extension('ui-select')
                 require('telescope').load_extension('project')
             end,
