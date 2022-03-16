ac93f2c80f57884633c1ec3b67433087d42d0457
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-03-16 22:02:47 +0300

    awesomewm: decrease spacing

--- dot_config/awesome/rc.lua
+++ dot_config/awesome/rc.lua
@@ -372,7 +372,7 @@ awful.screen.connect_for_each_screen(function(s)
         s.mytasklist, -- Middle widget
         { -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            spacing = 9,
+            spacing = 5,
             current_dm_version,
             redminer_widget,
             pushlocker_widget,
