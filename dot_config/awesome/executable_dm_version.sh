ef90b7a2dc55158da07853459e91efc7614196b6
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-03-18 10:07:13 +0300

    awesomewm: fix get dm_version on local machine

--- dot_config/awesome/executable_dm_version.sh
+++ dot_config/awesome/executable_dm_version.sh
@@ -4,4 +4,9 @@ if ! ip route list exact 192.168.161.0/24 | grep -q '^192.168.161.0/24'; then
 	exit 0
 fi
 
-ssh work cat /home/builder/SDKS/SSD/APPLY/DIR_QEMU_DBG/output/build/deuteron-master/dm/dm/dm_version.dm 2>/dev/null
+# sshfs
+if mount | grep -q '/home/builder type autofs'; then
+	ssh work cat /home/builder/SDKS/SSD/APPLY/DIR_QEMU_DBG/output/build/deuteron-master/dm/dm/dm_version.dm 2>/dev/null
+else
+	cat /home/builder/SDKS/SSD/APPLY/DIR_QEMU_DBG/output/build/deuteron-master/dm/dm/dm_version.dm 2>/dev/null
+fi
