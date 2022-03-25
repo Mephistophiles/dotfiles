d5d494a98f797349dffa45423e1203e606d29d2a
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-03-25 11:34:29 +0300

    neovim, cmp: use default kind comparator

--- dot_config/nvim/lua/settings/cmp.lua
+++ dot_config/nvim/lua/settings/cmp.lua
@@ -2,23 +2,6 @@ local M = {}
 
 function M.setup()
     local cmp = require 'cmp'
-    local types = require 'cmp.types'
-
-    local cmp_kind = function(entry1, entry2)
-        local kind1 = entry1:get_kind()
-        kind1 = kind1 == types.lsp.CompletionItemKind.Text and 100 or kind1
-        local kind2 = entry2:get_kind()
-        kind2 = kind2 == types.lsp.CompletionItemKind.Text and 100 or kind2
-        if kind1 ~= kind2 then
-            local diff = kind1 - kind2
-            if diff < 0 then
-                return true
-            elseif diff > 0 then
-                return false
-            end
-        end
-    end
-
     local lspkind = require 'lspkind'
     local source_mapping = {
         buffer = '[Buffer]',
@@ -100,7 +83,7 @@ function M.setup()
                 cmp.config.compare.offset, -- offset
                 cmp.config.compare.exact, -- exact
                 cmp.config.compare.score, -- score (priority based)
-                cmp_kind, -- kind based
+                cmp.config.compare.kind, -- kind based
                 cmp.config.compare.sort_text, -- text based (alpha sort)
                 cmp.config.compare.length, -- length sort
                 cmp.config.compare.order, -- source order sort
