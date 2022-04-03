56fbbe37a4b20cebfb763a0f420bdf4fb98ad637
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-04-03 21:34:49 +0300

    neovim: add orgmode

--- dot_config/nvim/lua/settings/treesitter.lua
+++ dot_config/nvim/lua/settings/treesitter.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 function M.setup()
+    require('orgmode').setup_ts_grammar()
     require('nvim-treesitter.configs').setup {
         autopairs = { enable = true },
         -- ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
@@ -15,6 +16,7 @@ function M.setup()
             'julia', --
             'lua', --
             'make', --
+            'org', --
             'python', --
             'query', --
             'rust', --
@@ -24,12 +26,12 @@ function M.setup()
         ignore_install = {}, -- List of parsers to ignore installing
         highlight = {
             enable = true, -- false will disable the whole extension
-            disable = {}, -- list of language that will be disabled
+            disable = { 'org' }, -- list of language that will be disabled
             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
             -- Using this option may slow down your editor, and you may see some duplicate highlights.
             -- Instead of true it can also be a list of languages
-            additional_vim_regex_highlighting = false,
+            additional_vim_regex_highlighting = { 'org' }, -- Required since TS highlighter doesn't support all syntax features (conceal)
         },
         textobjects = {
             select = {
