02bb6e2f5c09486653eefb8d42c3eb8792346703
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-04-16 19:52:53 +0300

    neovim: add incremental selection powered by treesitter

--- dot_config/nvim/lua/settings/treesitter.lua
+++ dot_config/nvim/lua/settings/treesitter.lua
@@ -33,6 +33,15 @@ function M.setup()
             -- Instead of true it can also be a list of languages
             additional_vim_regex_highlighting = { 'org' }, -- Required since TS highlighter doesn't support all syntax features (conceal)
         },
+        incremental_selection = {
+            enable = true,
+            keymaps = {
+                init_selection = 'gnn',
+                node_incremental = 'grn',
+                scope_incremental = 'grc',
+                node_decremental = 'grm',
+            },
+        },
         textobjects = {
             select = {
                 enable = true,
