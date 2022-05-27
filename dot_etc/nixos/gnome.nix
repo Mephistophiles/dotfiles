de5cfe404c46cc3e4ec94eb0382f83951c4938b2
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-05-27 16:46:27 +0300

    nixos: switch to paperwm for gnome 42
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- dot_etc/nixos/gnome.nix
+++ dot_etc/nixos/gnome.nix
@@ -20,18 +20,17 @@
             rev = "b66aaf13e8f4cdf0e2f9078fb3e75703535b822c";
             sha256 = "sha256-6AUUu63oWxRw9Wpxe0f7xvt7iilvQfhpAB8SYG4yP8Q=";
           };
-          # patches = old.patches or [ ] ++ [
-          #   (self.fetchpatch {
-          #     name = "pr-25.patch";
-          #     url =
-          #       "https://patch-diff.githubusercontent.com/raw/PaperWM-community/PaperWM/pull/25.patch";
-          #     sha256 = "sha256-ISIL3bmNICf6I91LxGGmNXhIyehEF+7W9gfL8bq+g6M=";
-          #   })
-          # ];
+          patches = old.patches or [ ] ++ [
+            (self.fetchpatch {
+              name = "pr-25.patch";
+              url =
+                "https://patch-diff.githubusercontent.com/raw/PaperWM-community/PaperWM/pull/25.patch";
+              sha256 = "sha256-ISIL3bmNICf6I91LxGGmNXhIyehEF+7W9gfL8bq+g6M=";
+            })
+          ];
         });
       };
     })
-
   ];
 
   environment.gnome.excludePackages = (with pkgs; [ gnome-photos gnome-tour ])
@@ -50,7 +49,10 @@
       atomix # puzzle game
     ]);
 
-  programs = { dconf.enable = true; gpaste.enable = true; };
+  programs = {
+    dconf.enable = true;
+    gpaste.enable = true;
+  };
   environment.systemPackages = with pkgs; [
     gnome3.adwaita-icon-theme
     gnomeExtensions.caffeine
