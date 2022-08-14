09d2eb4088397f0749236feee7021fa45466632e
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-08-14 16:50:23 +0300

    neovim: add new test plugin - cool-substitute
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -62,6 +62,30 @@ return packer.startup {
         }
 
         use {
+            'otavioschwanck/cool-substitute.nvim',
+            config = function()
+                require('cool-substitute').setup {
+                    setup_keybindings = true,
+                    -- mappings = {
+                    --   start = 'gm', -- Mark word / region
+                    --   start_and_edit = 'gM', -- Mark word / region and also edit
+                    --   start_and_edit_word = 'g!M', -- Mark word / region and also edit.  Edit only full word.
+                    --   start_word = 'g!m', -- Mark word / region. Edit only full word
+                    --   apply_substitute_and_next = 'M', -- Start substitution / Go to next substitution
+                    --   apply_substitute_and_prev = '<C-b>', -- same as M but backwards
+                    --   apply_substitute_all = 'ga', -- Substitute all
+                    --   force_terminate_substitute = 'g!!' -- Terminate macro (if some bug happens)
+                    -- },
+                    -- reg_char = 'o', -- letter to save macro (Dont use number or uppercase here)
+                    -- mark_char = 't', -- mark the position at start of macro
+                    -- writing_substitution_color = "#ECBE7B", -- for status line
+                    -- applying_substitution_color = "#98be65", -- for status line
+                    -- edit_word_when_starting_with_substitute_key = true (press M to mark and edit when not executing anything anything)
+                }
+            end,
+        }
+
+        use {
             'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
             module = 'lsp_lines',
             setup = function()
