cfae865440173b02066bcb397fe0fb1904558b47
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-08-16 16:26:50 +0300

    neovim, express_line: prettify
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- dot_config/nvim/lua/settings/express_line.lua
+++ dot_config/nvim/lua/settings/express_line.lua
@@ -10,6 +10,9 @@ function M.config()
     local sections = require 'el.sections'
     local subscribe = require 'el.subscribe'
 
+    local sep_left = ' ❯ '
+    local sep_right = ' ❮ '
+
     local diagnostic_formatter = function(_, _, counts)
         local items = {}
 
@@ -33,6 +36,10 @@ function M.config()
             highlight('DiagnosticHint', 'H', counts.hints)
         end
 
+        if #items > 0 then
+            table.insert(items, 1, sep_left)
+        end
+
         return table.concat(items, ' ')
     end
 
@@ -40,7 +47,7 @@ function M.config()
     local git_branch = subscribe.buf_autocmd('el_git_branch', 'BufEnter', function(window, buffer)
         local branch = extensions.git_branch(window, buffer)
         if branch then
-            return ' ' .. extensions.git_icon() .. ' ' .. branch
+            return sep_left .. extensions.git_icon() .. ' ' .. branch
         end
     end)
     local git_changes = subscribe.buf_autocmd('el_git_changes', 'BufWritePost', function(window, buffer)
@@ -55,33 +62,46 @@ function M.config()
         return lsp_statusline.current_function(window, buffer)
     end
 
+    local filetype = function(_, buffer)
+        local filetype = buffer.filetype
+        local icon = require('nvim-web-devicons').get_icon(buffer.name, buffer.extension, { default = true })
+
+        return icon .. ' ' .. filetype
+    end
+
     require('el').setup {
         -- An example generator can be seen in `Setup`.
         -- A default one is supplied if you do not want to customize it.
         generator = function()
             return {
-                extensions.gen_mode { format_string = ' %s ' },
-                git_branch,
-                diagnostic_display,
-                ' ',
+                sections.left_subsection {
+                    divider = sep_left,
+                    items = {
+                        extensions.gen_mode { format_string = ' %s ' },
+                        git_branch,
+                        diagnostic_display,
+                    },
+                },
                 sections.split,
                 sections.maximum_width(builtin.file_relative, 0.60),
                 sections.collapse_builtin { { ' ' }, { builtin.modified_flag } },
                 sections.split,
                 show_current_func,
                 git_changes,
-                '[',
-                builtin.line_with_width(3),
-                ':',
-                builtin.column_with_width(2),
-                ']',
                 sections.collapse_builtin {
-                    '[',
                     builtin.help_list,
                     builtin.readonly_list,
+                },
+                sep_right,
+                sections.collapse_builtin {
+                    '[',
+                    builtin.line_with_width(3),
+                    ':',
+                    builtin.column_with_width(2),
                     ']',
                 },
-                builtin.filetype,
+                sep_right,
+                filetype,
             }
         end,
     }
