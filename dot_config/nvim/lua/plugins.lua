58be94b5bcd1ad70154228f04c42960fc1823a60
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-10-01 19:36:26 +0300

    todo-comments: add support to highlight KEYWORD(username)

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -753,9 +753,26 @@ return packer.startup {
             requires = 'nvim-lua/plenary.nvim',
             config = function()
                 require('todo-comments').setup {
-                    -- your configuration comes here
-                    -- or leave it empty to use the default settings
-                    -- refer to the configuration section below
+                    highlight = {
+                        before = '', -- "fg" or "bg" or empty
+                        keyword = 'bg', -- "fg", "bg", "wide" or empty. (wide is the same as bg, but will also highlight surrounding characters)
+                        after = 'fg', -- "fg" or "bg" or empty
+                        pattern = [[.*<(KEYWORDS)(\([^\)]*\))?:]],
+                        comments_only = true, -- uses treesitter to match keywords in comments only
+                        max_line_len = 400, -- ignore lines longer than this
+                        exclude = {}, -- list of file types to exclude highlighting
+                    },
+                    search = {
+                        command = 'rg',
+                        args = {
+                            '--color=never',
+                            '--no-heading',
+                            '--with-filename',
+                            '--line-number',
+                            '--column',
+                        },
+                        pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]],
+                    },
                 }
             end,
         }
