19de44f20471f89c666be0c7a9339f185701cc9d
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-10-09 22:05:19 +0300

    nixos: configure autorandr
    Signed-off-by: Maxim Zhukov <mussitantesmortem@gmail.com>

--- dot_etc/nixos/configuration.nix
+++ dot_etc/nixos/configuration.nix
@@ -103,7 +103,65 @@ in
     };
 
     upower = { enable = true; };
-    autorandr = { enable = true; };
+    autorandr = {
+      enable = true;
+      profiles = {
+        laptop = {
+          fingerprint = {
+            eDP = "00ffffffffffff000dae0416000000000b1d0104a524147802ee95a3544c99260f505400000001010101010101010101010101010101b43b804a713834403020350063c71000001a000000fe004e3136314843412d4541330a20000000fe00434d4e0a202020202020202020000000fe004e3136314843412d4541330a200030";
+          };
+          config = {
+            eDP = {
+              enable = true;
+              primary = true;
+              position = "0x0";
+              mode = "1920x1080";
+            };
+          };
+        };
+        "docked-dp0" = {
+          fingerprint = {
+            "DisplayPort-0" = "00ffffffffffff0022f6256a22f680c01a200103805021780a54b1ac5047a426115054bfcf0081408180714f81c0b300d1c0010101014ed470a0d0a0465030403a001d4e3100001ce77c70a0d0a0295030203a001d4e3100001a000000fd0030901ea03c010a202020202020000000fc005a51452d4342410a202020202001b4020344b14b1f9014010304121305025a83010000e200d567030c001000384067d85dc4017880006d1a000002013090e60000000000e305c081e30f0000e606050166661c565e00a0a0a02950302035001d4e3100001a9d6770a0d0a0225030203a001d4e3100001a464714a0a0381f40302018041d4e3100001a00000000009b";
+          };
+          config = {
+            "eDP" = {
+              enable = false;
+            };
+            "DisplayPort-0" = {
+              enable = true;
+              primary = true;
+              position = "0x0";
+              mode = "3440x1440";
+            };
+          };
+        };
+        "docked-dp1" = {
+          fingerprint = {
+            "DisplayPort-1" = "00ffffffffffff0022f6256a22f680c01a200103805021780a54b1ac5047a426115054bfcf0081408180714f81c0b300d1c0010101014ed470a0d0a0465030403a001d4e3100001ce77c70a0d0a0295030203a001d4e3100001a000000fd0030901ea03c010a202020202020000000fc005a51452d4342410a202020202001b4020344b14b1f9014010304121305025a83010000e200d567030c001000384067d85dc4017880006d1a000002013090e60000000000e305c081e30f0000e606050166661c565e00a0a0a02950302035001d4e3100001a9d6770a0d0a0225030203a001d4e3100001a464714a0a0381f40302018041d4e3100001a00000000009b";
+          };
+          config = {
+            "eDP" = {
+              enable = false;
+            };
+            "DisplayPort-1" = {
+              enable = true;
+              primary = true;
+              position = "0x0";
+              mode = "3440x1440";
+            };
+          };
+        };
+      };
+    };
+    acpid = {
+      enable = true;
+      handlers = {
+        LID = {
+          action = "${pkgs.autorandr}/bin/autorandr --batch --change --default default";
+          event = "button/lid.*";
+        };
+      };
+    };
   };
 
   systemd = {
@@ -210,7 +268,7 @@ in
     gnome3.gnome-disk-utility
     gnome3.gnome-themes-extra
     gnome3.nautilus
-    josm
+    unstable.josm
     libreoffice
     meld
     thunderbird
