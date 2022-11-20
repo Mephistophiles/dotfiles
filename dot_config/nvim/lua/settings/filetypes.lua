f2bad18b1a90669576dab50a1527d0e6663025c9
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-11-20 15:45:16 +0300

    neovim: remove old type mappings

--- dot_config/nvim/lua/settings/filetypes.lua
+++ dot_config/nvim/lua/settings/filetypes.lua
@@ -6,24 +6,9 @@ function M.config()
             extensions = {
                 tl = 'teal',
             },
-            function_extensions = {
-                ['dm'] = function()
-                    vim.bo.filetype = 'json'
-                    vim.bo.shiftwidth = 4
-                    vim.bo.textwidth = 4
-                    vim.bo.expandtab = true
-                end,
-            },
             function_complex = {
                 ['Config.in'] = function()
                     vim.bo.filetype = 'kconfig'
-                end,
-                ['.*_config.default'] = function()
-                    vim.bo.filetype = 'json'
-                    vim.bo.shiftwidth = 4
-                    vim.bo.textwidth = 4
-                    vim.bo.expandtab = true
-                    vim.b.formatter_sort_keys = true
                 end,
             },
         },
