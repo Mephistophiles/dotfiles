3b4dfdf0b8d10e73afc5df720780ec263769856f
Maxim Zhukov <mussitantesmortem@gmail.com>
2022-11-25 15:58:43 +0300

    neovim: replace indent-o-matic to guess-indent.nvim

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -492,17 +492,9 @@ return packer.startup {
         }
 
         use {
-            'Darazaki/indent-o-matic',
+            'NMAC427/guess-indent.nvim',
             config = function()
-                require('indent-o-matic').setup {
-                    -- The values indicated here are the defaults
-
-                    -- Number of lines without indentation before giving up (use -1 for infinite)
-                    max_lines = 2048,
-
-                    -- Space indentations that should be detected
-                    standard_widths = { 2, 4, 8 },
-                }
+                require('guess-indent').setup {}
             end,
         }
 
