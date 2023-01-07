da2aa832c0d517f41f4308190724ba275f91733f
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-01-07 14:50:36 +0300

    neovim: add multi_rg function for telescope (stolen from tj)

--- dot_config/nvim/lua/settings/telescope.lua
+++ dot_config/nvim/lua/settings/telescope.lua
@@ -12,15 +12,84 @@ local themes = setmetatable({}, {
     end,
 })
 
+function telescope.multi_rg(opts)
+    local conf = require('telescope.config').values
+    local finders = require 'telescope.finders'
+    local make_entry = require 'telescope.make_entry'
+    local pickers = require 'telescope.pickers'
+
+    local flatten = vim.tbl_flatten
+
+    opts = opts or {}
+    opts.cwd = opts.cwd and vim.fn.expand(opts.cwd) or vim.loop.cwd()
+    opts.shortcuts = opts.shortcuts
+        or {
+            ['l'] = '*.lua',
+            ['v'] = '*.vim',
+            ['n'] = '*.{vim,lua}',
+            ['c'] = '*.c',
+        }
+    opts.pattern = opts.pattern or '%s'
+
+    local custom_grep = finders.new_async_job {
+        command_generator = function(prompt)
+            if not prompt or prompt == '' then
+                return nil
+            end
+
+            local prompt_split = vim.split(prompt, '  ')
+
+            local args = { 'rg' }
+            if prompt_split[1] then
+                table.insert(args, '--regexp')
+                table.insert(args, prompt_split[1])
+            end
+
+            if prompt_split[2] then
+                table.insert(args, '--glob')
+
+                local pattern
+                if opts.shortcuts[prompt_split[2]] then
+                    pattern = opts.shortcuts[prompt_split[2]]
+                else
+                    pattern = prompt_split[2]
+                end
+
+                table.insert(args, string.format(opts.pattern, pattern))
+            end
+
+            return flatten {
+                args,
+                { '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case' },
+            }
+        end,
+        entry_maker = make_entry.gen_from_vimgrep(opts),
+        cwd = opts.cwd,
+    }
+
+    pickers
+        .new(opts, {
+            debounce = 100,
+            prompt_title = 'Live Grep (with shortcuts)',
+            finder = custom_grep,
+            previewer = conf.grep_previewer(opts),
+            sorter = require('telescope.sorters').empty(),
+        })
+        :find()
+end
+
 function M.mappings()
     vim.keymap.set('n', '<C-p>', function()
+        telescope.find_files(themes.get_ivy {})
+    end, { desc = 'Fuzzy find files in project' })
+    vim.keymap.set('n', '<leader>ld', function()
         telescope.find_files(themes.get_ivy {})
     end, { desc = 'Fuzzy find files in project' })
     vim.keymap.set('n', '<C-b>', function()
         telescope.buffers(themes.get_ivy {})
     end, { desc = 'Fuzzy find opened buffers' })
     vim.keymap.set('n', '<leader>lg', function()
-        telescope.live_grep(themes.get_ivy {})
+        telescope.multi_rg(themes.get_ivy {})
     end, { desc = 'Fuzzy find with live grep' })
     vim.keymap.set('n', '<M-/>', function()
         telescope.current_buffer_fuzzy_find(themes.get_ivy {})
