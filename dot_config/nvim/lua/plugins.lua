2620f6e9f13e72a5d253fac207eed35f226ab0e2
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-01-11 20:44:39 +0300

    neovim: fix comment.nvim load

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -507,7 +507,7 @@ require('lazy').setup {
 
     { -- Smart and powerful comment plugin for neovim. Supports treesitter, dot repeat, left-right/up-down motions, hooks, and more
         'numToStr/Comment.nvim',
-        event = 'InsertEnter',
+        event = { 'VeryLazy', 'InsertCharPre' },
         config = function()
             require('Comment').setup()
         end,
