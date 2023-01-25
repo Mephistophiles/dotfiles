3078c66d29874ec78faba3e4b03a935266fffb5a
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-01-25 14:31:41 +0300

    neovim: cleanup mappings

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -15,9 +15,6 @@ end
 -- toggle spelling
 vim.keymap.set('n', '<F7>', [[:set spell!<CR>]], { desc = 'Toggle spelling' })
 
--- Back search for f F t T
-vim.keymap.set('n', [[\]], ',', { desc = 'Back search for f/F t/T' })
-
 -- paste signoff
 vim.keymap.set({ 'n', 'i' }, '<F8>', function()
     paste_git_signoff()
@@ -30,15 +27,6 @@ vim.keymap.set('c', '<C-p>', '<Up>', { desc = 'Select previous command' })
 vim.keymap.set('c', '<C-n>', '<Down>', { desc = 'Select next command' })
 vim.keymap.set('c', '<C-b>', '<Left>', { desc = 'Move cursor left' })
 vim.keymap.set('c', '<C-f>', '<Right>', { desc = 'Move cursor right' })
-
--- map Home to buffernext, End bufferprev
-vim.keymap.set('n', '<Home>', ':bnext', { desc = 'Select next buffer' })
-vim.keymap.set('n', '<End>', ':bprev', { desc = 'Select previous buffer' })
-
--- very magic search by default
-vim.keymap.set('n', [[?]], [[?\v]], { desc = 'Backward search with magic mode' })
-vim.keymap.set('n', [[/]], [[/\v]], { desc = 'Search with magic mode' })
-vim.keymap.set('c', [[%s/]], [[%sm/]], { desc = 'Search with magic mode' })
 
 -- Suppress command mistakes
 vim.api.nvim_create_user_command('W', 'w', { desc = 'Fix typo :W -> :w' })
@@ -67,12 +55,14 @@ vim.keymap.set('n', 'N', 'Nzz', { desc = 'Goto to previous search with possible 
 
 -- copy in clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Copy to clipboard' })
-vim.keymap.set('n', '<leader>Y', 'gg"+yG', { desc = 'Copy to clipboard all file' })
+vim.keymap.set('n', '<leader>Y', '+y$', { desc = 'Copy to clipboard all file' })
 
--- workarounds
--- inoremap <C-c> <Esc>
-vim.keymap.set('i', '<C-c>', '<Esc>', { desc = 'Emulate <ESC> by Ctrl+C' })
+-- tab/buffer/quickfix
+vim.keymap.set('n', ']t', CMD 'tabnext', { desc = 'Tabs: goto next tab' })
+vim.keymap.set('n', '[t', CMD 'tabprev', { desc = 'Tabs: goto previous tab' })
 
--- quickfix
-vim.keymap.set('n', ']q', ':cnext<CR>', { desc = 'QuickFix: goto next error' })
-vim.keymap.set('n', '[q', ':cprev<CR>', { desc = 'QuickFix: goto previous error' })
+vim.keymap.set('n', ']b', CMD 'bnext', { desc = 'Buffers: goto next buffer' })
+vim.keymap.set('n', '[b', CMD 'bprev', { desc = 'Buffers: goto previous buffer' })
+
+vim.keymap.set('n', ']q', CMD 'cnext', { desc = 'QuickFix: goto next error' })
+vim.keymap.set('n', '[q', CMD 'cprev', { desc = 'QuickFix: goto previous error' })
