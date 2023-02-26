473cfa9292fa275d74ba5ac574c552cf36618ea1
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-02-26 17:36:04 +0300

    nixos: enable python lsp

--- dot_etc/nixos/configuration.nix
+++ dot_etc/nixos/configuration.nix
@@ -352,7 +352,7 @@ in
     unstable.nodePackages.vscode-json-languageserver
     unstable.nodePackages.jsonlint
     unstable.nodePackages.vls
-    # unstable.python310Packages.python-lsp-server
+    unstable.python310Packages.python-lsp-server
     # unstable.vscode-extensions.llvm-org.lldb-vscode
 
     # file systems
