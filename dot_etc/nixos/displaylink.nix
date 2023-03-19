6ce854a5d476146397246648901ee7070487e6a5
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-03-19 14:55:13 +0300

    nixos: format docs

--- dot_etc/nixos/displaylink.nix
+++ dot_etc/nixos/displaylink.nix
@@ -1,21 +1,21 @@
 {
-      linux-override = final: prev: {
-        linuxPackages_latest =
-          prev.linuxPackages_latest.extend
-            (lpfinal: lpprev: {
-              evdi =
-                lpprev.evdi.overrideAttrs (efinal: eprev: {
-                  version = "1.12.0-git";
-                  src = prev.fetchFromGitHub {
-                    owner = "DisplayLink";
-                    repo = "evdi";
-                    rev = "bdc258b25df4d00f222fde0e3c5003bf88ef17b5";
-                    sha256 = "sha256-mt+vEp9FFf7smmE2PzuH/3EYl7h89RBN1zTVvv2qJ/o=";
-                  };
-                });
+  linux-override = final: prev: {
+    linuxPackages_latest =
+      prev.linuxPackages_latest.extend
+        (lpfinal: lpprev: {
+          evdi =
+            lpprev.evdi.overrideAttrs (efinal: eprev: {
+              version = "1.12.0-git";
+              src = prev.fetchFromGitHub {
+                owner = "DisplayLink";
+                repo = "evdi";
+                rev = "bdc258b25df4d00f222fde0e3c5003bf88ef17b5";
+                sha256 = "sha256-mt+vEp9FFf7smmE2PzuH/3EYl7h89RBN1zTVvv2qJ/o=";
+              };
             });
-        displaylink = prev.displaylink.override {
-          inherit (final.linuxPackages_latest) evdi;
-        };
-      };
+        });
+    displaylink = prev.displaylink.override {
+      inherit (final.linuxPackages_latest) evdi;
+    };
+  };
 }
