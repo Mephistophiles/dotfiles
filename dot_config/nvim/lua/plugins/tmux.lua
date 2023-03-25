438120f2c4ace849848a262b2590908b28bd826e
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-03-25 14:11:45 +0300

    neovim: fix lazy load for tmux.nvim

--- dot_config/nvim/lua/plugins/tmux.lua
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -2,11 +2,13 @@ return { -- tmux integration for nvim features pane movement and resizing from w
     'aserowy/tmux.nvim',
     keys = {
         { '<c-h>', CMD 'lua require("tmux").move_left()', desc = 'Left movement' },
+        { '<c-j>', CMD 'lua require("tmux").move_bottom()', desc = 'Bottom movement' },
+        { '<c-k>', CMD 'lua require("tmux").move_top()', desc = 'Top movement' },
         { '<c-l>', CMD 'lua require("tmux").move_right()', desc = 'Right movement' },
-        { '<a-h>', CMD 'lua require("tmux").move_left()', desc = 'Left movement' },
-        { '<a-j>', CMD 'lua require("tmux").move_bottom()', desc = 'Bottom movement' },
-        { '<a-k>', CMD 'lua require("tmux").move_top()', desc = 'Top movement' },
-        { '<a-l>', CMD 'lua require("tmux").move_right()', desc = 'Right movement' },
+        { '<a-h>', CMD 'lua require("tmux").resize_left()', desc = 'Resize to left' },
+        { '<a-j>', CMD 'lua require("tmux").resize_bottom()', desc = 'Resize to bottom' },
+        { '<a-k>', CMD 'lua require("tmux").resize_top()', desc = 'Resize to top' },
+        { '<a-l>', CMD 'lua require("tmux").resize_right()', desc = 'Resize to right' },
     },
     config = function()
         require('tmux').setup {
