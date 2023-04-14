8be4df278acf8a13ea888bf4d2f60394f598d273
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-04-14 14:05:35 +0300

    neovim, noice: use default cmdline instead of popup to better user experience with code completion

--- dot_config/nvim/lua/plugins/noice.lua
+++ dot_config/nvim/lua/plugins/noice.lua
@@ -2,7 +2,7 @@ return {
     'folke/noice.nvim',
     config = {
         cmdline = {
-            -- view = 'cmdline',
+            view = 'cmdline',
         },
         lsp = {
             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
@@ -15,7 +15,7 @@ return {
         -- you can enable a preset for easier configuration
         presets = {
             bottom_search = true, -- use a classic bottom cmdline for search
-            command_palette = true, -- position the cmdline and popupmenu together
+            command_palette = false, -- position the cmdline and popupmenu together
             long_message_to_split = true, -- long messages will be sent to a split
             inc_rename = false, -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = false, -- add a border to hover docs and signature help
@@ -38,32 +38,6 @@ return {
         -- OPTIONAL:
         --   `nvim-notify` is only needed, if you want to use the notification view.
         --   If not available, we use `mini` as the fallback
-        {
-            'rcarriga/nvim-notify',
-            config = function()
-                local notify_fn = require 'notify'
-                vim.notify = setmetatable({}, {
-                    __call = function(_, msg, level, opts)
-                        if vim.g.nvim_notify_stack_trace and level and level >= vim.log.levels.WARN then
-                            msg = msg .. '\n' .. debug.traceback('Trace', 2)
-                        end
-
-                        notify_fn(msg, level, opts)
-                    end,
-                    __index = function(_, key)
-                        return notify_fn[key]
-                    end,
-                })
-
-                vim.keymap.set('n', '<F3>', function()
-                    vim.g.nvim_notify_stack_trace = not vim.g.nvim_notify_stack_trace
-                    vim.notify(
-                        'Stacktrace in notifications was '
-                            .. (vim.g.nvim_notify_stack_trace and 'enabled' or 'disabled'),
-                        vim.log.levels.INFO
-                    )
-                end, { desc = 'Notifications: toggle stacktrace from warn and above' })
-            end,
-        },
+        'rcarriga/nvim-notify',
     },
 }
