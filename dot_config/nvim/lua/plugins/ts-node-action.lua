0477509a9d9ec9abb703341e1f20c52a7725a7a8
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-05-22 22:24:30 +0300

    neovim: use leader-leader for change symbol under cursor

--- dot_config/nvim/lua/plugins/ts-node-action.lua
+++ dot_config/nvim/lua/plugins/ts-node-action.lua
@@ -2,7 +2,7 @@ return { --  Neovim Plugin for running functions on nodes.
     'ckolkey/ts-node-action',
     keys = {
         {
-            '-',
+            '<leader><leader>',
             function()
                 require('ts-node-action').node_action()
             end,
