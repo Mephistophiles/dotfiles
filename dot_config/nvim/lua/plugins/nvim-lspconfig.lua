97362f095e2eafc90ae67293001e046a74d39900
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-05-29 20:47:31 +0300

    neovim: refactor

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,11 +1,9 @@
 local utils = require 'plugins.utils.lsp'
 
-local exe_resolve = function(name, default)
+local exe_resolve = function(name)
     if vim.fn.exepath(name) then
         return name
     end
-
-    return default
 end
 
 local setup_server = function(server, config)
@@ -36,41 +34,34 @@ return {
         name = 'lspconfig',
         config = function()
             local servers = {
-                lua_ls = function()
-                    local runtime_path = vim.split(package.path, ';')
-                    table.insert(runtime_path, 'lua/?.lua')
-                    table.insert(runtime_path, 'lua/?/init.lua')
-
-                    return {
-                        settings = {
-                            Lua = {
-                                runtime = {
-                                    -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-                                    version = 'LuaJIT',
-
-                                    -- Setup your lua path
-                                    path = runtime_path,
-                                },
-                                diagnostics = {
-                                    -- Get the language server to recognize the `vim` global
-                                    globals = { 'vim' },
-                                },
-                                workspace = {
-                                    -- Make the server aware of Neovim runtime files
-                                    library = vim.api.nvim_get_runtime_file('', true),
-                                },
-                                -- Do not send telemetry data containing a randomized but unique identifier
-                                telemetry = { enable = false },
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            runtime = {
+                                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                                version = 'LuaJIT',
+                            },
+                            diagnostics = {
+                                -- Get the language server to recognize the `vim` global
+                                globals = { 'vim' },
+                            },
+                            workspace = {
+                                -- Make the server aware of Neovim runtime files
+                                library = vim.api.nvim_get_runtime_file('', true),
+                            },
+                            -- Do not send telemetry data containing a randomized but unique identifier
+                            telemetry = {
+                                enable = false,
                             },
                         },
-                    }
-                end,
+                    },
+                },
                 -- rust_analyzer = true, -- via rust-tools
                 clangd = { capabilities = { offsetEncoding = { 'utf-16' } } },
                 gopls = true,
                 hls = true,
                 jsonls = {
-                    cmd = { exe_resolve('vscode-json-languageserver', 'vscode-json-language-server'), '--stdio' },
+                    cmd = { exe_resolve 'vscode-json-languageserver' or 'vscode-json-language-server', '--stdio' },
                 },
                 pylsp = true,
                 pyright = true,
