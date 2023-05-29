d0b1497b313081476577f89cf9ad1738bf3c3674
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-05-29 18:39:33 +0300

    neovim: replace express_line with lualine as more generic

---
+++ dot_config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,34 @@
+return {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+        require('lualine').setup {
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = {
+                    'branch',
+                    'diff',
+                    { 'diagnostics', symbols = { error = 'E', warn = 'W', info = 'I', hint = 'H' } },
+                },
+                lualine_c = { { 'filename', path = 1 } },
+                lualine_x = {
+                    {
+                        require('noice').api.status.mode.get,
+                        cond = require('noice').api.status.mode.has,
+                        color = { fg = '#ff9e64' },
+                    },
+                    {
+                        require('noice').api.status.search.get,
+                        cond = require('noice').api.status.search.has,
+                        color = { fg = '#ff9e64' },
+                    },
+                    'encoding',
+                    'fileformat',
+                    'filetype',
+                },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location', 'selectioncount' },
+            },
+        }
+    end,
+}
