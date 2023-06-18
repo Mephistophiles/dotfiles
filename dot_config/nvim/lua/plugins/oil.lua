8f735d6378388a37aafde804a06ff8e62179933e
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-06-18 19:37:41 +0300

    neovim, oil: use <leader>p for preview mode

--- dot_config/nvim/lua/plugins/oil.lua
+++ dot_config/nvim/lua/plugins/oil.lua
@@ -5,8 +5,24 @@ return {
             view_options = {
                 show_hidden = true,
             },
+            keymaps = {
+                ['g?'] = 'actions.show_help',
+                ['<CR>'] = 'actions.select',
+                ['<C-v>'] = 'actions.select_vsplit',
+                ['<C-s>'] = 'actions.select_split',
+                ['<C-t>'] = 'actions.select_tab',
+                ['<leader>p'] = 'actions.preview',
+                ['<C-c>'] = 'actions.close',
+                ['<C-l>'] = 'actions.refresh',
+                ['-'] = 'actions.parent',
+                ['_'] = 'actions.open_cwd',
+                ['`'] = 'actions.cd',
+                ['~'] = 'actions.tcd',
+                ['g.'] = 'actions.toggle_hidden',
+            },
+            use_default_keymaps = false,
         }
-        vim.keymap.set('n', '-', require('oil').open, { desc = 'Open parent directory' })
+        vim.keymap.set('n', '-', require('oil').open_float, { desc = 'Open parent directory' })
     end,
     -- Optional dependencies
     dependencies = { 'nvim-tree/nvim-web-devicons' },
