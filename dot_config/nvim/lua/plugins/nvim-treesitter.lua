54e273c076bacd88c74abbe037a7a071aa710529
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-07-19 19:43:40 +0300

    neovim: move treesj close to treesitter

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -194,4 +194,17 @@ return {
         cmd = 'TSPlaygroundToggle',
         dependencies = { 'nvim-treesitter/nvim-treesitter' },
     },
+    {
+        'Wansmer/treesj',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        cmd = { 'TSJToggle', 'TSJSplit', 'TSJJoin' },
+        keys = {
+            {
+                '<space>j',
+                CMD 'TSJToggle',
+                desc = 'TreeSJ: toggle node under cursor (split if one-line and join if multiline)',
+            },
+        },
+        opts = {},
+    },
 }
