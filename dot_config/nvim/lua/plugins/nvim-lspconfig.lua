20072c489c62cd36df6b8a6fd5eec7f54f7ee563
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-08-03 12:44:19 +0300

    neovim, lsp, clangd: enable clang-tidy at check

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -228,7 +228,21 @@ return {
                     },
                 },
                 -- rust_analyzer = true, -- via rust-tools
-                clangd = { capabilities = { offsetEncoding = { 'utf-16' } } },
+                clangd = {
+                    cmd = {
+                        -- see clangd --help-hidden
+                        'clangd',
+                        '--background-index',
+                        -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+                        -- to add more checks, create .clang-tidy file in the root directory
+                        -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
+                        '--clang-tidy',
+                        '--completion-style=bundled',
+                        '--cross-file-rename',
+                        '--header-insertion=iwyu',
+                    },
+                    capabilities = { offsetEncoding = { 'utf-16' } },
+                },
                 gopls = true,
                 jsonls = {
                     cmd = { exe_resolve 'vscode-json-languageserver' or 'vscode-json-language-server', '--stdio' },
