e92810ded2fbd39b17212f508c3ae7511fef5479
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-09-02 11:56:20 +0300

    neovim: remove vim-indent-object

--- dot_config/nvim/lua/plugins/vim-indent-object.lua
+++
@@ -1,3 +0,0 @@
-return { -- Vim plugin that defines a new text object representing lines of code at the same indent level. Useful for python/vim scripts, etc.
-    'michaeljsmith/vim-indent-object',
-}
