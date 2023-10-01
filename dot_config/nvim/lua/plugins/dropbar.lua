98614070110b834a48634d011cde73612eac3c8c
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-10-01 19:37:59 +0300

    neovim: add dropbar plugin

---
+++ dot_config/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'Bekaboo/dropbar.nvim',
+    enabled = function()
+      local version = vim.version()
+      version = {version.major, version.minor, version.patch}
+      return vim.version.cmp(version, {0, 10, 0}) > -1
+    end
+    -- optional, but required for fuzzy finder support
+    -- dependencies = {
+    --   'nvim-telescope/telescope-fzf-native'
+    -- }
+  }
+}
