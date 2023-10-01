0a3aa99ff63377395b1fedfdadf8e40186ec19b7
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-10-01 19:39:29 +0300

    neovim: update to v3 version

--- dot_config/nvim/lua/plugins/indent-blankline.lua
+++ dot_config/nvim/lua/plugins/indent-blankline.lua
@@ -1,11 +1,6 @@
 return { -- Indent guides for Neovim
     'lukas-reineke/indent-blankline.nvim',
     event = 'VeryLazy',
-    config = function()
-        require('indent_blankline').setup {
-            space_char_blankline = ' ',
-            show_current_context = false,
-            show_current_context_start = false,
-        }
-    end,
+    main = 'ibl',
+    opts = {},
 }
