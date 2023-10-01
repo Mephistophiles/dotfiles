16b96c0a81766116eceb95f4f619254274bc22a6
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-10-01 19:25:32 +0300

    neovim: disable format on save by default

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -168,7 +168,7 @@ return {
             -- Call setup() LAST!
             require('guard').setup {
                 -- the only options for the setup function
-                fmt_on_save = true,
+                fmt_on_save = false,
                 -- Use lsp if no formatter was defined for this filetype
                 lsp_as_default_formatter = false,
             }
