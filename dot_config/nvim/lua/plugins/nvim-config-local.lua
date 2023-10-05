6734d645055ef498c40e85201db3de542f6bd874
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-10-05 20:52:21 +0300

    neovim: format code

--- dot_config/nvim/lua/plugins/nvim-config-local.lua
+++ dot_config/nvim/lua/plugins/nvim-config-local.lua
@@ -1,20 +1,19 @@
-return 
-{
-  "klen/nvim-config-local",
-  config = function()
-    require('config-local').setup {
-      -- Default options (optional)
+return {
+    'klen/nvim-config-local',
+    config = function()
+        require('config-local').setup {
+            -- Default options (optional)
 
-      -- Config file patterns to load (lua supported)
-      config_files = { ".nvim.lua", ".nvimrc", ".exrc" },
+            -- Config file patterns to load (lua supported)
+            config_files = { '.nvim.lua', '.nvimrc', '.exrc' },
 
-      -- Where the plugin keeps files data
-      hashfile = vim.fn.stdpath("data") .. "/config-local",
+            -- Where the plugin keeps files data
+            hashfile = vim.fn.stdpath 'data' .. '/config-local',
 
-      autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
-      commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
-      silent = true,             -- Disable plugin messages (Config loaded/ignored)
-      lookup_parents = false,     -- Lookup config files in parent directories
-    }
-  end
+            autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+            commands_create = true, -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
+            silent = true, -- Disable plugin messages (Config loaded/ignored)
+            lookup_parents = false, -- Lookup config files in parent directories
+        }
+    end,
 }
