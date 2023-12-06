de91f4107a71c4e6ca421a7d91d65380cbb343a0
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-12-06 11:00:21 +0300

    neovim: use LspAttach event for symbol-usage

--- dot_config/nvim/lua/plugins/symbol-usage.lua
+++ dot_config/nvim/lua/plugins/symbol-usage.lua
@@ -1,6 +1,6 @@
 return {
     'Wansmer/symbol-usage.nvim',
-    event = 'BufReadPre', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+    event = 'LspAttach', -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
     config = function()
         require('symbol-usage').setup { vt_position = 'end_of_line' }
     end,
