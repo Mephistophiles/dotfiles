df9ebf3c37bdcb3afcc6ac0cedccbf5702a28892
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-12-07 11:35:43 +0300

    neovim: disable diffview

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,6 @@
 return { -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
     'sindrets/diffview.nvim',
+    enabled = false,
     dependencies = 'nvim-tree/nvim-web-devicons',
     cmd = { 'DiffviewOpen' },
 }
