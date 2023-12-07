5310764bef58635f85a079eb553ec48128ca7fed
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-12-07 11:36:04 +0300

    neovim: disable numb

--- dot_config/nvim/lua/plugins/numb.lua
+++ dot_config/nvim/lua/plugins/numb.lua
@@ -1,5 +1,6 @@
 return { --  Peek lines just when you intend
     'nacro90/numb.nvim',
+    enabled = false,
     event = { 'CmdlineChanged', 'CmdlineLeave' },
     config = function()
         require('numb').setup()
