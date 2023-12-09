c8a2894da43e10112558a1400adf0b84054a2895
Maxim Zhukov <mussitantesmortem@gmail.com>
2023-12-09 17:49:58 +0300

    neovim: remove unused deps for pretty-fold

--- dot_config/nvim/lua/plugins/pretty-fold.lua
+++ dot_config/nvim/lua/plugins/pretty-fold.lua
@@ -3,7 +3,5 @@ return { -- Foldtext customization in Neovim
     event = 'VeryLazy',
     config = function()
         require('pretty-fold').setup {}
-        require('fold-preview').setup()
     end,
-    dependencies = { 'anuvyklack/nvim-keymap-amend', 'anuvyklack/fold-preview.nvim' },
 }
