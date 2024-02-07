eae621f4e4d776c64dbea487b9b9b6e1e0ac8ede
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-02-07 19:07:26 +0300

    neovim: harpoon: migrate to 2 version

--- dot_config/nvim/lua/plugins/harpoon.lua
+++ dot_config/nvim/lua/plugins/harpoon.lua
@@ -1,62 +1,99 @@
 return { -- Quick change harpooned buffers
     'ThePrimeagen/harpoon',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-lua/popup.nvim' },
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = true,
+    opts = {},
     keys = {
         {
             '<Leader>`',
             function()
-                require('harpoon.ui').toggle_quick_menu()
+                local harpoon = require 'harpoon'
+
+                harpoon.ui:toggle_quick_menu(harpoon:list())
             end,
             desc = 'Harpoon: show menu',
         },
         {
             '<Leader>h',
             function()
-                require('harpoon.mark').add_file()
+                local harpoon = require 'harpoon'
+
+                harpoon:list():append()
             end,
             desc = 'Harpoon: toogle mark',
         },
         {
             '<Leader>1',
             function()
-                require('harpoon.ui').nav_file(1)
+                local harpoon = require 'harpoon'
+
+                harpoon:list():select(1)
             end,
             desc = 'Harpoon: select 1 mark',
         },
         {
             '<Leader>2',
             function()
-                require('harpoon.ui').nav_file(2)
+                local harpoon = require 'harpoon'
+
+                harpoon:list():select(2)
             end,
             desc = 'Harpoon: select 2 mark',
         },
         {
             '<Leader>3',
             function()
-                require('harpoon.ui').nav_file(3)
+                local harpoon = require 'harpoon'
+
+                harpoon:list():select(3)
             end,
             desc = 'Harpoon: select 3 mark',
         },
         {
             '<Leader>4',
             function()
-                require('harpoon.ui').nav_file(4)
+                local harpoon = require 'harpoon'
+
+                harpoon:list():select(4)
             end,
             desc = 'Harpoon: select 4 mark',
         },
         {
             '<Leader>5',
             function()
-                require('harpoon.ui').nav_file(5)
+                local harpoon = require 'harpoon'
+
+                harpoon:list():select(5)
             end,
             desc = 'Harpoon: select 5 mark',
         },
         {
             '<Leader>6',
             function()
-                require('harpoon.ui').nav_file(6)
+                local harpoon = require 'harpoon'
+
+                harpoon:list():select(6)
             end,
             desc = 'Harpoon: select 6 mark',
+        },
+        {
+            '<C-S-P>',
+            function()
+                local harpoon = require 'harpoon'
+
+                harpoon:list():prev()
+            end,
+            desc = 'Harpoon: Toggle previous buffers stored within list',
+        },
+        {
+            '<C-S-N>',
+            function()
+                local harpoon = require 'harpoon'
+
+                harpoon:list():next()
+            end,
+            desc = 'Harpoon: Toggle next buffers stored within list',
         },
     },
 }
