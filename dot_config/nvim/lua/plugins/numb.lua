de3a2f4f33f588c32f2caf6fe29effdd98f5d03b
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-02-26 23:58:02 +0300

    neovim: remove unused plugins

--- dot_config/nvim/lua/plugins/numb.lua
+++
@@ -1,8 +0,0 @@
-return { --  Peek lines just when you intend
-    'nacro90/numb.nvim',
-    enabled = false,
-    event = { 'CmdlineChanged', 'CmdlineLeave' },
-    config = function()
-        require('numb').setup()
-    end,
-}
