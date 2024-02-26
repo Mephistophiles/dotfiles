14235ba11ff4fbd23b5b870d54c3eebda2798b1d
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-02-26 23:54:11 +0300

    neovim: treesitter-context: limit context lines

--- dot_config/nvim/lua/plugins/nvim-treesitter.lua
+++ dot_config/nvim/lua/plugins/nvim-treesitter.lua
@@ -116,12 +116,12 @@ return {
             vim.wo.foldmethod = 'expr'
         end,
         dependencies = {
-            'nvim-treesitter/nvim-treesitter-textobjects',
+            { 'nvim-treesitter/nvim-treesitter-textobjects' },
             {
                 'nvim-treesitter/nvim-treesitter-context',
                 config = {
                     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-                    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                    max_lines = 15, -- How many lines the window should span. Values <= 0 mean no limit.
                     min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
                     line_numbers = true,
                     multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
