bc505b29cdaf8c1477bbed5214f7a0d860ead4e3
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-02-26 23:54:27 +0300

    neovim: ts-node-action: add support to toggle custom booleans

--- dot_config/nvim/lua/plugins/ts-node-action.lua
+++ dot_config/nvim/lua/plugins/ts-node-action.lua
@@ -11,6 +11,25 @@ return { --  Neovim Plugin for running functions on nodes.
     },
     dependencies = { 'nvim-treesitter' },
     config = function() -- Optional
-        require('ts-node-action').setup {}
+        local helpers = require 'ts-node-action.helpers'
+        local pairs = {
+            rtl_true = 'rtl_false',
+            rtl_false = 'rtl_true',
+        }
+
+        local function switch(node)
+            local node_text = helpers.node_text(node)
+            return pairs[node_text] or node_text
+        end
+        require('ts-node-action').setup {
+            c = {
+                identifier = {
+                    {
+                        switch,
+                        name = 'Toggle Pairs',
+                    },
+                },
+            },
+        }
     end,
 }
