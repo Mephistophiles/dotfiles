fcbdab1958eec92166a12b63e76d843eb00c5a58
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-03-04 09:53:39 +0300

    neovim: replace snippets for luals

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -48,6 +48,9 @@ local supported_languages = {
             return {
                 settings = {
                     Lua = {
+                        completion = {
+                            callSnippet = 'Replace',
+                        },
                         runtime = {
                             -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                             version = 'LuaJIT',
