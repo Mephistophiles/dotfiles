9a5999c8bf9176e48b23793b0c52f3c9421beee6
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-03-10 10:56:20 +0300

    neovim: add neotest

---
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,39 @@
+return {
+    'nvim-neotest/neotest',
+    keys = {
+        { '<leader>tr', CMD 'lua require("neotest").run.run()', desc = 'Neotest: run' },
+        {
+            '<leader>tf',
+            CMD 'lua require("neotest").run.run(vim.fn.expand("%"))',
+            desc = 'Neotest: run in current file',
+        },
+        {
+            '<leader>ts',
+            CMD 'lua require("neotest").run.stop()',
+            desc = 'Neotest: stop',
+        },
+        { '<leader>tl', CMD 'lua require("neotest").run.run_last()', desc = 'Neotest: run last' },
+        { '<leader>ts', CMD 'lua require("neotest").summary.toggle()', desc = 'Neotest: show summary' },
+        {
+            '<leader>to',
+            CMD 'lua require("neotest").output.open({ enter = true, auto_close = true })',
+            desc = 'Neotest: show test output',
+        },
+        {
+            '<leader>tO',
+            CMD 'lua require("neotest").output_panel.toggle()',
+            desc = 'Neotest: show test output',
+        },
+    },
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+    },
+    config = function()
+        require('neotest').setup {
+            adapters = {
+                require 'rustaceanvim.neotest',
+            },
+        }
+    end,
+}
