1f36ef32b12f87253b1267e8974610e81552c741
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-03-15 15:39:04 +0300

    neovim: remove keys from table mode

--- dot_config/nvim/lua/plugins/vim-table-mode.lua
+++ dot_config/nvim/lua/plugins/vim-table-mode.lua
@@ -1,7 +1,4 @@
 return { -- VIM Table Mode for instant table creation.
     'dhruvasagar/vim-table-mode',
-    keys = {
-        { '<leader>tm', CMD 'TableModeToggle', desc = 'TableMode: toggle' },
-    },
     cmd = { 'TableModeToggle' },
 }
