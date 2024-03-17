d34dc3cc0f456e49d818192dd0a6ebf7e491b21c
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-03-17 08:21:15 +0300

    fish: add just completions

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -35,6 +35,7 @@ fish_add_path $HOME/go/bin/
 starship init fish | source
 zoxide init fish | source
 direnv hook fish | source
+just --completions fish | source
 
 function fish_user_key_bindings
   fzf_key_bindings
