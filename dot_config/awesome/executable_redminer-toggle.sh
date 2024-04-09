4f25b7a19e6a0c581ce286c7f0546d310303d91b
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-04-09 15:52:28 +0300

    awesome: remove old scripts
    Change-Id: I92f7f8abb5439a4d3c5d370ad4d51805b5cc33b8

--- dot_config/awesome/executable_redminer-toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if redminer timer status date today active | grep -q "Running"; then
-	redminer timer suspend
-elif redminer timer status date today active | grep -q "Suspended"; then
-	redminer timer resume
-fi
--- dot_config/awesome/executable_redminer.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-export PATH="$PATH:$HOME/bin"
-SCRIPT_DIR=$(dirname $(realpath $0))
-lua $SCRIPT_DIR/redminer.lua
