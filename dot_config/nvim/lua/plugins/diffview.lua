0c2f9a2801bc4aaa66bc2805404d8559f62d8909
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-04-09 15:53:42 +0300

    neovim, diffview: add support to load diffviewfilehistory
    Change-Id: I45d5414ce13f1bb29dd72d1e1374da0cf42250a6

--- dot_config/nvim/lua/plugins/diffview.lua
+++ dot_config/nvim/lua/plugins/diffview.lua
@@ -1,5 +1,5 @@
 return { -- Single tabpage interface for easily cycling through diffs for all modified files for any git rev.
     'sindrets/diffview.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
-    cmd = { 'DiffviewOpen' },
+    cmd = { 'DiffviewOpen', 'DiffviewFileHistory' },
 }
