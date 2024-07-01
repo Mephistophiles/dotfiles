e5ff1ab8d44079191e50aea7aa809d13b93587df
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-07-01 22:24:39 +0300

    neovim: disable neorg

--- dot_config/nvim/lua/plugins/neorg.lua
+++ dot_config/nvim/lua/plugins/neorg.lua
@@ -2,6 +2,7 @@ return {
     'nvim-neorg/neorg',
     dependencies = { { 'vhyrro/luarocks.nvim', priority = 1000, config = true }, 'nvim-treesitter' },
     ft = 'norg',
+    enabled = false,
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = '*', -- Pin Neorg to the latest stable release
     config = function()
