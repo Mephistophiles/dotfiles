80e72b0f4defbc14de978d8e75c0e677b6a26669
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-12-09 15:50:50 +0300

    neovim, snacks: remove lsp progress and add support to scratch buffer
    Change-Id: Id6bf3abc32f04633724de824d65644948b990294

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -1,47 +1,3 @@
-local progress = vim.defaulttable()
-
-vim.api.nvim_create_autocmd('LspProgress', {
-    ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        local value = ev.data.params.value --[[@as {percentage?: number, title?: string, message?: string, kind: "begin" | "report" | "end"}]]
-        if not client or type(value) ~= 'table' then
-            return
-        end
-        local p = progress[client.id]
-
-        for i = 1, #p + 1 do
-            if i == #p + 1 or p[i].token == ev.data.params.token then
-                p[i] = {
-                    token = ev.data.params.token,
-                    msg = ('[%3d%%] %s%s'):format(
-                        value.kind == 'end' and 100 or value.percentage or 100,
-                        value.title or '',
-                        value.message and (' **%s**'):format(value.message) or ''
-                    ),
-                    done = value.kind == 'end',
-                }
-                break
-            end
-        end
-
-        local msg = {} ---@type string[]
-        progress[client.id] = vim.tbl_filter(function(v)
-            return table.insert(msg, v.msg) or not v.done
-        end, p)
-
-        local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
-        Snacks.notifier(table.concat(msg, '\n'), 'info', {
-            id = 'lsp_progress',
-            title = client.name,
-            opts = function(notif)
-                notif.icon = #progress[client.id] == 0 and ' '
-                    or spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
-            end,
-        })
-    end,
-})
-
 return {
     'folke/snacks.nvim',
     priority = 1000,
@@ -78,6 +34,20 @@ return {
                 Snacks.terminal()
             end,
             desc = 'Snacks: toggle term',
+        },
+        {
+            '<leader>-',
+            function()
+                Snacks.scratch()
+            end,
+            desc = 'Snacks: toggle Scratch Buffer',
+        },
+        {
+            '<leader>_',
+            function()
+                Snacks.scratch.select()
+            end,
+            desc = 'Snacks: select Scratch Buffer',
         },
     },
     config = function()
