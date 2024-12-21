ae55f6e2374344a6b27cfc1ef110d12e6924b97a
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-12-21 13:56:49 +0300

    neovim, lspconfig; use static table instead of function
    Change-Id: Ief301fa1b5088e279248301154409fc1855b2a3b

--- dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,22 +1,20 @@
 local clangd = {
     'clangd',
-    function()
-        return {
-            cmd = {
-                -- see clangd --help-hidden
-                'clangd',
-                '--background-index',
-                -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
-                -- to add more checks, create .clang-tidy file in the root directory
-                -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
-                '--clang-tidy',
-                '--completion-style=bundled',
-                '--cross-file-rename',
-                '--header-insertion=iwyu',
-            },
-            capabilities = { offsetEncoding = { 'utf-16' } },
-        }
-    end,
+    {
+        cmd = {
+            -- see clangd --help-hidden
+            'clangd',
+            '--background-index',
+            -- by default, clang-tidy use -checks=clang-diagnostic-*,clang-analyzer-*
+            -- to add more checks, create .clang-tidy file in the root directory
+            -- and add Checks key, see https://clang.llvm.org/extra/clang-tidy/
+            '--clang-tidy',
+            '--completion-style=bundled',
+            '--cross-file-rename',
+            '--header-insertion=iwyu',
+        },
+        capabilities = { offsetEncoding = { 'utf-16' } },
+    },
 }
 
 local supported_languages = {
