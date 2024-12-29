2132bd43e389152d583359495e6886c8b51a7c3e
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-12-29 20:55:01 +0300

    neovim, fileline: enable only if filename with colons
    Change-Id: I2ee297908416bbb181bf7466932027c52c8939d4

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -51,5 +51,20 @@ return {
         },
     },
     { 'stevearc/profile.nvim', lazy = true },
-    { 'lewis6991/fileline.nvim' },
+    {
+        'lewis6991/fileline.nvim',
+        event = { 'BufNewFile' },
+        cond = function()
+            local file = vim.api.nvim_buf_get_name(0)
+
+            if file == '' then
+                return false
+            end
+
+            if not file:match '^([^:]+):([0-9:]+)$' then
+                return false
+            end
+            return true
+        end,
+    },
 }
