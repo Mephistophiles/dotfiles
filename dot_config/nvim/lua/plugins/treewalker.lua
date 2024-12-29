7f163f6c1c919d37f1a1dbf49ebe025cba25523a
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-12-29 20:55:14 +0300

    neovim, treewalker: use swap movements
    Change-Id: I5662d6bb4b903a84bf703187fea68d3edb8bf7a2

--- dot_config/nvim/lua/plugins/treewalker.lua
+++ dot_config/nvim/lua/plugins/treewalker.lua
@@ -5,6 +5,8 @@ return {
         { '<a-j>', CMD 'Treewalker Down', desc = 'Treewalker: down movement' },
         { '<a-k>', CMD 'Treewalker Up', desc = 'Treewalker: up movement' },
         { '<a-l>', CMD 'Treewalker Right', desc = 'Treewalker: right movement' },
+        { '<c-a-j>', CMD 'Treewalker SwapDown', desc = 'Treewalker: swap down' },
+        { '<c-a-k>', CMD 'Treewalker SwapUp', desc = 'Treewalker: swap up' },
     },
     opts = {
         highlight = true, -- Whether to briefly highlight the node after jumping to it
