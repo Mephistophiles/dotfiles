3a471eba8d6cb6979c40086f1112f46f43c9e523
Maxim Zhukov <mussitantesmortem@gmail.com>
2024-12-31 14:25:30 +0300

    neovim; use default completion preset

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -13,13 +13,9 @@ return {
         -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
         -- see the "default configuration" section below for full documentation on how to define
         -- your own keymap.
-        keymap = { preset = 'super-tab', ['<C-y>'] = { 'select_and_accept' } },
+        keymap = { preset = 'default' },
 
         appearance = {
-            -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-            -- Useful for when your theme doesn't support blink.cmp
-            -- will be removed in a future release
-            use_nvim_cmp_as_default = true,
             -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
             -- Adjusts spacing to ensure icons are aligned
             nerd_font_variant = 'mono',
