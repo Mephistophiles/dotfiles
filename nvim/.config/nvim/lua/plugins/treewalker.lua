7f34d926ab919e4475a31455c01879a3c9b8ae34
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-01-21 16:23:53 +0300

    neovim: treewalker: add swap left/right keymaps
    Change-Id: I5ede7557c62306b516022f6690ea2993d5493a76

--- nvim/.config/nvim/lua/plugins/treewalker.lua
+++ nvim/.config/nvim/lua/plugins/treewalker.lua
@@ -5,8 +5,10 @@ return {
         { '<a-j>', CMD 'Treewalker Down', desc = 'Treewalker: down movement' },
         { '<a-k>', CMD 'Treewalker Up', desc = 'Treewalker: up movement' },
         { '<a-l>', CMD 'Treewalker Right', desc = 'Treewalker: right movement' },
+        { '<c-a-h>', CMD 'Treewalker SwapLeft', desc = 'Treewalker: swap left' },
         { '<c-a-j>', CMD 'Treewalker SwapDown', desc = 'Treewalker: swap down' },
         { '<c-a-k>', CMD 'Treewalker SwapUp', desc = 'Treewalker: swap up' },
+        { '<c-a-l>', CMD 'Treewalker SwapRight', desc = 'Treewalker: swap right' },
     },
     opts = {
         highlight = true, -- Whether to briefly highlight the node after jumping to it
