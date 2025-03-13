895b1454d119907416f4320e27970425aa03f684
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-03-13 16:20:24 +0300

    neovim, flash: add support to word hop
    Change-Id: Ieb514b5a89e8e32277bfe571e1e8bdd992a1f773

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,3 +1,12 @@
+---@param opts Flash.Format
+local function format(opts)
+    -- always show first and second label
+    return {
+        { opts.match.label1, 'FlashMatch' },
+        { opts.match.label2, 'FlashLabel' },
+    }
+end
+
 return {
     'folke/flash.nvim',
     ---@type Flash.Config
@@ -10,6 +19,67 @@ return {
                 require('flash').jump()
             end,
             desc = 'Flash: motion',
+        },
+        {
+            '<leader>w',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').jump {
+                    pattern = '.', -- initialize pattern with any char
+                    search = {
+                        mode = function(pattern)
+                            -- remove leading dot
+                            if pattern:sub(1, 1) == '.' then
+                                pattern = pattern:sub(2)
+                            end
+                            -- return word pattern and proper skip pattern
+                            return ([[\<%s\w*\>]]):format(pattern), ([[\<%s]]):format(pattern)
+                        end,
+                    },
+                    -- select the range
+                    jump = { pos = 'range' },
+                }
+            end,
+            desc = 'Flash: word motion',
+        },
+        {
+            '<leader>W',
+            mode = { 'n', 'x', 'o' },
+            function()
+                require('flash').jump {
+                    search = { mode = 'search' },
+                    label = { after = false, before = { 0, 0 }, uppercase = false, format = format },
+                    pattern = [[\<]],
+                    action = function(match, state)
+                        state:hide()
+                        require('flash').jump {
+                            search = { max_length = 0 },
+                            highlight = { matches = false },
+                            label = { format = format },
+                            matcher = function(win)
+                                -- limit matches to the current label
+                                return vim.tbl_filter(function(m)
+                                    return m.label == match.label and m.win == win
+                                end, state.results)
+                            end,
+                            labeler = function(matches)
+                                for _, m in ipairs(matches) do
+                                    m.label = m.label2 -- use the second label
+                                end
+                            end,
+                        }
+                    end,
+                    labeler = function(matches, state)
+                        local labels = state:labels()
+                        for m, match in ipairs(matches) do
+                            match.label1 = labels[math.floor((m - 1) / #labels) + 1]
+                            match.label2 = labels[(m - 1) % #labels + 1]
+                            match.label = match.label1
+                        end
+                    end,
+                }
+            end,
+            desc = 'Flash: 2 char hop motion',
         },
         {
             '<leader>v',
