d414b1f6631e1a533d1fe31dfc5bab81fc674939
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-03-25 16:44:47 +0300

    wezterm: add support to switch between tabs
    Change-Id: I83ca55254f5989867da536ec0ac4455eefbb6a5f

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -75,6 +75,11 @@ return {
 		{ key = "Insert", mods = "CTRL", action = act.CopyTo("PrimarySelection") },
 		{ key = "Copy", mods = "NONE", action = act.CopyTo("Clipboard") },
 		{ key = "Paste", mods = "NONE", action = act.PasteFrom("Clipboard") },
+		{ key = "1", mods = "CTRL|ALT", action = act.ActivateTab(0) },
+		{ key = "2", mods = "CTRL|ALT", action = act.ActivateTab(1) },
+		{ key = "3", mods = "CTRL|ALT", action = act.ActivateTab(2) },
+		{ key = "4", mods = "CTRL|ALT", action = act.ActivateTab(3) },
+		{ key = "5", mods = "CTRL|ALT", action = act.ActivateTab(4) },
 	},
 
 	key_tables = {
