bf19a0e4f117a4620cda84d6f8c84c5b34e5fe21
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-04-03 17:49:05 +0300

    neovim: remove unused todo comments
    Change-Id: I14abc89895c754dd5fb51ee80f3dbac001647333

--- nvim/.config/nvim/lua/plugins/todo-comments.lua
+++
@@ -1,9 +0,0 @@
-return { -- Highlight, list and search todo comments in your projects
-    'folke/todo-comments.nvim',
-    event = 'VeryLazy',
-    dependencies = 'nvim-lua/plenary.nvim',
-    opts = {
-        highlight = { pattern = [[.*<((KEYWORDS)%(\(.{-1,}\))?):]] },
-        search = { pattern = [[\b(KEYWORDS)(\([^\)]*\))?:]] },
-    },
-}
