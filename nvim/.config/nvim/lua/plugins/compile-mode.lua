6a6f61e2600adb0bcb48b36ca82095af330cc4c4
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-05-12 22:04:01 +0300

    neovim: add new commands for compile mode
    Change-Id: I7c9e612549a8705c1a8a997bde464319c267a2d4

--- nvim/.config/nvim/lua/plugins/compile-mode.lua
+++ nvim/.config/nvim/lua/plugins/compile-mode.lua
@@ -7,7 +7,7 @@ return {
             '<F5>',
             CMD 'Recompile',
             mode = { 'n' },
-            desc = 'Overseer: run last action',
+            desc = 'Compile-Mode: run last action',
         },
     },
     dependencies = {
@@ -22,5 +22,40 @@ return {
             -- to add ANSI escape code support, add:
             baleia_setup = true,
         }
+
+        local function run_task(params, prepend)
+            local cmd = { vim.fn.expandcmd(params.args) }
+
+            if prepend then
+                table.insert(cmd, 1, prepend)
+            end
+
+            vim.g.compile_command = table.concat(cmd, ' ')
+
+            require('compile-mode').compile {
+                args = vim.g.compile_command,
+            }
+        end
+        vim.api.nvim_create_user_command('Run', function(params)
+            run_task(params)
+        end, {
+            desc = 'Compile-Mode: run compile task',
+            nargs = '*',
+            bang = true,
+        })
+        vim.api.nvim_create_user_command('Make', function(params)
+            run_task(params, 'make')
+        end, {
+            desc = 'Compile-Mode: run make target',
+            nargs = '*',
+            bang = true,
+        })
+        vim.api.nvim_create_user_command('Just', function(params)
+            run_task(params, 'just')
+        end, {
+            desc = 'Compile_mode: run justfile target',
+            nargs = '*',
+            bang = true,
+        })
     end,
 }
