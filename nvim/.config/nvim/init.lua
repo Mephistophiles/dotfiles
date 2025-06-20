bcb57795bd5e85d34d1db1afe2f35b52aa46abda
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-06-20 10:41:47 +0300

    neovim: use small project root replacement
    Change-Id: Icee5a8d90ad6cabc339bfa5e25504e70000cd833

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -31,6 +31,20 @@ vim.filetype.add {
     },
 }
 
+local group = vim.api.nvim_create_augroup('ProjectRoot', { clear = true })
+
+vim.api.nvim_create_autocmd({ 'VimEnter', 'BufEnter' }, {
+    pattern = '*',
+    group = group,
+    callback = function()
+        local project_root = vim.fs.root(0, { '.git', '.root' })
+
+        if project_root then
+            vim.api.nvim_set_current_dir(project_root)
+        end
+    end,
+})
+
 require 'globals'
 require 'filetype'
 require 'mappings'
