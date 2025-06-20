2f791c6b0466b16fd5b1707df8c8795528b0fcdf
Maxim Zhukov <mussitantesmortem@gmail.com>
2025-06-20 10:41:32 +0300

    neovim: remove unused code
    Change-Id: I7b8053d8a450142fa855e8d631235a8434b947ec

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -1,22 +1,8 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = vim.g.mapleader
 
-local function paste_git_signoff()
-    local username = io.popen('git config user.name', 'r'):read '*l'
-    local email = io.popen('git config user.email', 'r'):read '*l'
-
-    vim.api.nvim_put({ 'Signed-off-by: ' .. username .. ' <' .. email .. '>' }, 'c', false, true)
-
-    return true
-end
-
 -- toggle spelling
 vim.keymap.set('n', '<F7>', [[:set spell!<CR>]], { desc = 'Toggle spelling' })
-
--- paste signoff
-vim.keymap.set({ 'n', 'i' }, '<F8>', function()
-    paste_git_signoff()
-end, { desc = 'Paste Signed-off-by' })
 
 -- command mode helpers
 vim.keymap.set('c', '<C-a>', '<Home>', { desc = 'Goto to start of line' })
@@ -43,16 +29,6 @@ vim.keymap.set('n', 'N', 'Nzz', { desc = 'Goto to previous search with possible 
 -- copy in clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Copy to clipboard' })
 vim.keymap.set('n', '<leader>Y', '"+y$', { desc = 'Copy to clipboard all file' })
-
--- tab/buffer/quickfix
-vim.keymap.set('n', ']t', CMD 'tabnext', { desc = 'Tabs: goto next tab' })
-vim.keymap.set('n', '[t', CMD 'tabprev', { desc = 'Tabs: goto previous tab' })
-
-vim.keymap.set('n', ']b', CMD 'bnext', { desc = 'Buffers: goto next buffer' })
-vim.keymap.set('n', '[b', CMD 'bprev', { desc = 'Buffers: goto previous buffer' })
-
-vim.keymap.set('n', ']q', CMD 'cnext', { desc = 'QuickFix: goto next error' })
-vim.keymap.set('n', '[q', CMD 'cprev', { desc = 'QuickFix: goto previous error' })
 
 vim.keymap.set('i', '<C-c>', '<Esc>', { desc = 'Handle C-c as Esc, otherwise some operations can be aborted' })
 
